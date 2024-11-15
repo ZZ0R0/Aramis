b492ef7aef138bc6fad7cc66961fdcdb5bd7e32d
robin felez <robin@WIN-CLI-01.lab.local>
2024-11-15 18:07:52 +0100

    0.277

--- Payload_Type/horus/horus/agent_code/horus/commands/bases/pwd/BasePwd.cpp
+++ Payload_Type/horus/horus/agent_code/horus/commands/bases/pwd/BasePwd.cpp
@@ -1,7 +1,9 @@
 #include "BasePwd.h"
+#include <windows.h>
 
 BOOL basePwd(char* result, size_t resultSize)
 {
+    MessageBoxA(NULL, "basePwd function called", "Info", MB_OK);
     CustomStrCopy(result, currentDirectory, resultSize);
     return TRUE;
 }
