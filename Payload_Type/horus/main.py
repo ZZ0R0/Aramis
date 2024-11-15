c92304658984affbdb9b8d29de9712ad31dd2785
robin felez <robin@WIN-CLI-01.lab.local>
2024-11-15 18:59:09 +0100

    0.279

--- Payload_Type/horus/main.py
+++ Payload_Type/horus/main.py
@@ -1,7 +1,7 @@
 #from mywebhook.webhook import *
 import mythic_container
 import asyncio
-import horus
+from horus.mythic import *
 from translator.translator import *
 
 mythic_container.mythic_service.start_and_run_forever()
